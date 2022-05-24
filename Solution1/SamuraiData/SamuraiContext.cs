1c3e52d77ea031cd1f7ed4f4c6265faa379743a8
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-05-24 21:28:09 +0100

    We created DataModel and Added Nugget

---
+++ Solution1/SamuraiData/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiDomain;
+using System;
+
+namespace SamuraiData
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDb; Initial Catalog=SamuraiAppData");
+        }
+    }
+      
+    
+}
---
+++ Solution1/SamuraiDomain/Samurai.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiDomain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Quote> Quotes { get; set; } = new List<Quote>();
+
+       
+    }
+}
+// one to many relationship between Samurai And Quoote
